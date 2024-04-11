865f94fbedcbf9c9f799d855d6c59e973c9d1b73
kacparr <tvzator134@gmail.com>
2024-04-12 00:23:49 +0200

    make text to ascii and gif support

--- README.md
+++ README.md
@@ -2,20 +2,19 @@
 
 ### TODO
 
-* Dealing with spaghetti when changing to ascii
-* Moving the text into image (finding the right size etc) if image scaling will be implemented
+* refactoring (priority)
+  + seperate function for each type of conversion 
+  + prompts
 * Coloring the letters if text is moved
 * Trying to show the art in neofetch
 * Making the function to auto-generate ASCII letters in different styles
 * Making a website out of this
+* Text to ascii
 * Dockerise it?
 
 ### DONE
 * Image scaling for both x and y coords
-  + NVM! YOU ONLY NEED TO SCALE THE IMAGE !!!
-  + ~~the code is fucking spaghetti and slow (it was alright for some time but rn it has 0.3 seconds longer compute time)~~
-  + ~~it has to be only one loop instead of two, it creates complexity, we should have circa 0.04 compute time but we have 0.04^3 (while loop takes a lot of time)~~
-  + ~~possible solution is to only iterate through x and make x / scale_X arrays of pixels, and clear it on the scale_x time~~
-  + ~~da sie to zrobic jednym for loopem liczac ile bedzie razem przejść w obrazie na gridzie + jedno modulo i zwiekszajac x patrzac na y~~ 
   + numpy?
-  + ~~search function is completely unnecessary, but~~ 
+* Moving the text into image (finding the right size etc) if image scaling will be implemented
+  + done and works as expected (finds the right size of text)
+* GIFS 
--- ascii.py
+++ ascii.py
@@ -1,36 +1,45 @@
-from PIL import Image, ImageColor, ImageDraw, ImageFont
+from PIL import Image, ImageColor, ImageDraw, ImageFont, ImageSequence
 import time
 import numpy as np 
 start = time.time()
-def img_to_ascii():
-    symbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-    symbols_short = "@%#*+=-:. "
+def get_img(src, scale:int=1): # MODE
     ## opening the image
-    img = Image.open("src/sand.png")
+    img = Image.open(src)
+    img_format = img.format
     colorImg = img.copy()
-    img_white = Image.new("RGBA", img.size, "WHITE")
-    img_white.paste(img, (0, 0), img)
-    img = img_white
-    img.save("ahaha.png")
-    img = img.convert("L")
-    img = img.resize((int(img.size[0]), int(img.size[1])))
-    x,y = img.size
-    newImg = Image.new(mode="RGBA", size=((x*4)-1,(y*4)-1), color=(255,0,0,0))
-    font = ImageFont.truetype("iosevka.ttf",8) ## find em 
-
-    # finding depth by assigning ascii symbols to the greyscale
+    if img.format == "PNG":
+        img_white = Image.new("RGBA", img.size, "WHITE")
+        img_white.paste(img, (0, 0), img.convert("RGBA"))
+        img = img_white
+    img.format = img_format
+    if img.format == "GIF":
+        handle_gif(img, scale)
+    else:
+        img = img.convert("L")
+        img = img.resize((int(img.size[0]/scale), int(img.size[1]/scale)))
+        img_to_ascii(img)
+    
+def img_to_ascii(img:Image.Image):
+    # finding depth by assigning ascii symbols to greyscale
+    font = ImageFont.truetype("fonts/firamono.ttf", 16)
+    symbols = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+    # symbols_small = "@%#*+=-:. "
     scale_dict = dict()
     for i in range(len(symbols)):
-        if i == 69:
+        print(f"{symbols[i]} {font.getbbox(symbols[i])}")
+        if i == len(symbols) - 1:
             scale_dict[255] = symbols[i]
         else:
-            scale = int((235 / (len(symbols)-2) * i))
+            scale = int((225 / (len(symbols)-2) * i))
             scale_dict[scale] = symbols[i]
-    # print(scale_dict)
     vals = list(scale_dict.keys())
     ## MAIN FUNCTION
+    x,y = img.size
+    result = Image.new(mode="RGBA", size=((x),(y*8)), color="WHITE")
+
+    draw = ImageDraw.Draw(result)
+    
     data = np.asarray(img)
-    print(data)
     res = open("res.txt", "w")
     closest = {}
     index_y = 0
@@ -39,41 +48,33 @@ def img_to_ascii():
         line = ""
         while index_x < x: 
             pxl = data[index_y,index_x]
-            # print(colors)
-                ## convert
             if pxl in closest.keys():
                 v = closest[pxl] ##bottleneck?
             else:
                 v = search(pxl, vals)
+                # v = symbols[pxl * (len(symbols) -1) // 255]
                 closest[pxl] = v
-            res.write(scale_dict[v])
             line += scale_dict[v]
+            # line += v
             index_x += 1
+        res.write(line)
+        if int(font.getlength(line)) > result.width:
+            # print(f"{result.width}, {result.height}, {font.getlength(line)}")
+            new = Image.new(mode="RGBA",size=(int(font.getlength(line)), result.height), color="#ffffff")
+            new.paste(result, (0, 0))
+            result = new.copy()
+            draw = ImageDraw.Draw(result)
+        draw.text(xy=(0, index_y*8), text=line, font=font, fill="#000000", anchor="lt")
+
         res.write('\n')
         index_y+=1
-
-    # # print(vals)
-    # for i in range(y):
-    #     for j in range(x):
-    #         p = img.getpixel((j,i))[0]
-
-    #         else:
-    #             v = search(p, vals)
-    #             # print(f"pixel:{p}. closest:{v}")
-    #             closest[p] = v
-    #         res.write(scale_dict[v])
-    #         draw.text(xy=(j*4,i*4),text=scale_dict[v],font=font, fill="#000000")
-    #     res.write('\n')
-    # newImg.save("xd.png")
+    result.save("res.png")
 
 def search(x, A:list):
     #1. Dzielenie listy na 3 do optymalizacji
     A_i = ((0,9), (10,39), (40,70))
-
     x_mod = x // 100
     A = A[A_i[x_mod][0]:A_i[x_mod][1]]
-
-    # print(A)
     # i = prawa strona, mid dzieli array na 2, n = długość arraya
     i, mid, n = 0, 0, len(A)
     # sprawdzanie pierwszego i ostatniego indeksu
@@ -83,14 +84,11 @@ def search(x, A:list):
         return A[n-1]
     while i < n:
         mid = (i + n) // 2
-        # print(f"mid: {mid}, x={x}, n={n}")
-        # jeżeli idealnie mid
         if x == A[mid]:
             return A[mid]
 
         if x < A[mid]: # dla lewej strony
             if x > A[mid - 1] and mid > 0:
-                # print(A[mid-1], A[mid], x)
                 res = get_closest(A[mid -1], A[mid], x)
                 return res
             n = mid # mid się zmniejsza o mid ponieważ nic nie będzie mniejsze po prawej stronie
@@ -99,7 +97,7 @@ def search(x, A:list):
                 res = get_closest(A[mid + 1], A[mid], x)
                 return res
             i = mid + 1 # zwiększamy prawą stronę, i dąży do n
-        
+
     return A[mid] #jeżeli nie zostanie nic
         
 def get_closest(a,b,x):
@@ -107,7 +105,33 @@ def get_closest(a,b,x):
         return a
     return b
 
-img_to_ascii()
+    
+def handle_gif(img:Image.Image, scale): #color or not?
+    frames = []
+    duration = []
+    for frame in ImageSequence.Iterator(img):
+        frame = frame.convert("L")
+        frame = frame.resize((int(img.size[0]/scale), int(img.size[1]/scale)))
+        frames.append(img_to_ascii(frame))
+        duration.append(frame.info['duration'])
+        print(len(frames))
+    frames[0].save("xd2.gif", save_all=True, append_images=frames[1:], duration=duration, loop=img.info['loop'])
+   
+   
+## FUTURE FUNCTIONS FOR REFACTORING 
+def handle_text(img:Image.Image, scale):
+    pass # write res.txt
+
+def handle_img_color(img:Image.Image, scale):
+    pass # write res_rgb.png
+
+def handle_img_gray(img:Image.Image, scale):
+    pass # write res.png 
+
+def text_to_ascii(mode):
+    pass #future   
+    
+get_img("src/image.jpg", 2)
 end = time.time()
 print(end-start)
-            
+
